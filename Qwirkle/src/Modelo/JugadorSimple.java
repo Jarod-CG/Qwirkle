8d2302936f6c61b4f9c87d81846535c18df696d3
Jarod Cervantes Gutiérrez <jcervantesg05@gamil.com>
2020-09-29 09:31:47 -0600

    U

--- Qwirkle/src/Modelo/JugadorSimple.java
+++ Qwirkle/src/Modelo/JugadorSimple.java
@@ -5,10 +5,42 @@
  */
 package Modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Jarod
  */
 public class JugadorSimple extends Jugador{
+        //por ahora la lista es de pares
+    //no es de pares y valor
+    public ArrayList<int[]> jugar (Ficha[][] matriz, Ficha[] mano){
+        ArrayList<int[]> arr = new ArrayList();
+        arr.addAll(0, jugar_aux(matriz, mano, mano.length, new ArrayList()));
+        return arr;
+    }
     
+    
+    //[(2,3),(12,4),(0,1),null,(4,3)]
+    //integrar jugadaValida en el backtracking
+    public ArrayList<ArrayList<Jugada>> jugar_aux (Ficha[][] matriz, Ficha[] mano, int num, ArrayList<ArrayList<Jugada>> solucion) {
+        if (num==0){
+            return solucion;
+        }
+        else {
+            //recorre la mano
+            //posiblemente usar swap que usa el profe
+            for (int i = 0; i < num; i++) {
+                ArrayList<Jugada> posicionesMano = posiblesPosiciones(matriz, mano, i, solucion);
+                //ir probando cada uno de las posiciones de la ficha
+                for (int j = 0; j < posicionesMano.size(); j++) {
+                    //quizas aqui vaya el swap de fichas
+                    solucion.add(posicionesMano.get(j));
+                    jugar_aux (matriz, mano, num-1, solucion);
+                    //deshace el swap
+                }
+            }
+            return new ArrayList();
+        }
+    } 
 }
